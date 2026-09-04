4a85ab094bbed20fca3b836c2f7b03e9d01f7a8c
Fareedah <olaidefaridat@gmail.com>
2026-09-04 16:11:59 +0100

    Revert "new update"
    This reverts commit dd16a57f75ce970d89a2b4cacae34d39ef245b69.

--- src/components/common/Loader.jsx
+++
@@ -1,9 +0,0 @@
-function Loader({ size = "md", fullScreen = false }) {
-  const sizes = { sm: "h-4 w-4", md: "h-7 w-7", lg: "h-10 w-10" };
-  return (
-    <div className={fullScreen ? "flex min-h-screen items-center justify-center bg-background" : "flex items-center justify-center py-10"}>
-      <div className={`${sizes[size]} animate-spin rounded-full border-2 border-primary/15 border-t-accent`} />
-    </div>
-  );
-}
-export default Loader;
